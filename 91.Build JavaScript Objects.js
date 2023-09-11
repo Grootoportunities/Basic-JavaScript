d25c276f0cb7fb435dd9caefc5085e9af2514a90
Grootoportunities <tapir1337@mail.ru>
2023-09-11 20:06:27 +0300

    Create 91.Build JavaScript Objects.js

---
+++ 91.Build JavaScript Objects.js
@@ -0,0 +1,10 @@
+const myDog = {
+  // Only change code below this line
+
+  name: "Deryl",
+  legs: 4,
+  tails: 1,
+  friends: ["Me?", "Dad", "Mom", "Sister"],
+
+  // Only change code above this line
+};
