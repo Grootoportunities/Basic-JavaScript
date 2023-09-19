74ca32d071f0fd789b2e25a0b78712ac2d08d8c7
Grootoportunities <tapir1337@mail.ru>
2023-09-19 16:39:11 +0300

    Create 100.Manipulating Complex Objects.js

---
+++ 100.Manipulating Complex Objects.js
@@ -0,0 +1,16 @@
+const myMusic = [
+  {
+    artist: "Billy Joel",
+    title: "Piano Man",
+    release_year: 1973,
+    formats: ["CD", "8T", "LP"],
+    gold: true,
+  },
+  {
+    artist: "We Came As Romans",
+    title: "Black Hole",
+    release_year: 2023,
+    formats: ["CD", "Spotify", "LP"],
+    gold: true,
+  },
+];
